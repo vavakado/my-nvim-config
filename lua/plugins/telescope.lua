c78740ad65fcc42c32a9540df2c5e731859e6f50
Vladimir Rubin <vavakado@proton.me>
2024-06-22 23:10:57 +0300

    added telescope-zoxide

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,4 +22,29 @@ return {
 			require('telescope').load_extension 'ui-select'
 		end,
 	},
+	{
+		'jvgrootveld/telescope-zoxide',
+		config = function()
+			local z_utils = require 'telescope._extensions.zoxide.utils'
+
+			require('telescope').setup {
+				extensions = {
+					zoxide = {
+						prompt_title = '[ Zoxide ]',
+						mappings = {
+							default = {
+								after_action = function(selection)
+									print('Update to (' .. selection.z_score .. ') ' .. selection.path)
+								end,
+							},
+							-- Opens the selected entry in a new split
+							['<C-q>'] = { action = z_utils.create_basic_command 'split' },
+						},
+					},
+				},
+				-- require('telescope').load_extension "zoxide",
+			}
+			vim.keymap.set('n', '<Leader>fd', ':Telescope zoxide list<CR>', { desc = '[F]ind [D]irectories' })
+		end,
+	},
 }
--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -2,10 +2,82 @@ vim.o.clipboard = 'unnamedplus'
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
+
+-- relative numbers
 vim.o.nu = true
 vim.o.rnu = true
 
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.showmode = false
+
+vim.o.breakindent = true
+vim.o.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = 'yes'
+
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Live substitution
+vim.opt.inccommand = 'split'
+
+vim.o.scrolloff = 7
+
+
+vim.opt.hlsearch = true
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+if vim.g.neovide then
+	vim.o.guifont = 'Iosevka Nerd Font Mono:h15'
+end
+
+vim.keymap.set('', 'f', 'e')
+vim.keymap.set('', 'p', 'r')
+vim.keymap.set('', 'g', 't')
+vim.keymap.set('', 'j', 'y')
+vim.keymap.set('', 'l', 'u')
+vim.keymap.set('', 'u', 'i')
+vim.keymap.set('', 'y', 'o')
+vim.keymap.set('', 'r', 's')
+vim.keymap.set('', 's', 'd')
+vim.keymap.set('', 't', 'f')
+vim.keymap.set('', 'd', 'g')
+vim.keymap.set('', 'h', 'h')
+vim.keymap.set('', 'n', 'j')
+vim.keymap.set('', 'e', 'k')
+vim.keymap.set('', 'i', 'l')
+vim.keymap.set('', 'o', 'p')
+vim.keymap.set('', 'k', 'n')
+vim.keymap.set('', 'F', 'E')
+vim.keymap.set('', 'P', 'R')
+vim.keymap.set('', 'G', 'T')
+vim.keymap.set('', 'J', 'Y')
+vim.keymap.set('', 'L', 'U')
+vim.keymap.set('', 'U', 'I')
+vim.keymap.set('', 'Y', 'O')
+vim.keymap.set('', 'R', 'S')
+vim.keymap.set('', 'S', 'D')
+vim.keymap.set('', 'T', 'F')
+vim.keymap.set('', 'D', 'G')
+vim.keymap.set('', 'H', 'H')
+vim.keymap.set('', 'N', 'J')
+vim.keymap.set('', 'E', 'K')
+vim.keymap.set('', 'I', 'L')
+vim.keymap.set('', 'O', 'P')
+vim.keymap.set('', 'K', 'N')
+vim.o.langmap =
+	'ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz'
+vim.o.langremap = true
 
 if vim.g.neovide then
 	vim.o.guifont = 'Iosevka Nerd Font Mono:h15'
