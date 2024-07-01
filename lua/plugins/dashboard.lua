f458ec990d2ab9fd6584d176955d403f839dd062
Vladimir Rubin <vavakado@proton.me>
2024-07-01 14:04:04 +0300

    switch to alpha-nvim

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,66 +1,36 @@
 return {
-	'nvimdev/dashboard-nvim',
-	event = 'VimEnter',
+	'goolord/alpha-nvim',
 	config = function()
-		require('dashboard').setup {
-			theme = 'hyper',
-			shortcut_type = 'letter',
-			config = {
-				header = {
-					'⣿⠟⣽⣿⣿⣿⣿⣿⢣⠟⠋⡜⠄⢸⣿⣿⡟⣬⢁⠠⠁⣤⠄⢰⠄⠇⢻⢸',
-					'⢏⣾⣿⣿⣿⠿⣟⢁⡴⡀⡜⣠⣶⢸⣿⣿⢃⡇⠂⢁⣶⣦⣅⠈⠇⠄⢸⢸',
-					'⣹⣿⣿⣿⡗⣾⡟⡜⣵⠃⣴⣿⣿⢸⣿⣿⢸⠘⢰⣿⣿⣿⣿⡀⢱⠄⠨⢸',
-					'⣿⣿⣿⣿⡇⣿⢁⣾⣿⣾⣿⣿⣿⣿⣸⣿⡎⠐⠒⠚⠛⠛⠿⢧⠄⠄⢠⣼',
-					'⣿⣿⣿⣿⠃⠿⢸⡿⠭⠭⢽⣿⣿⣿⢂⣿⠃⣤⠄⠄⠄⠄⠄⠄⠄⠄⣿⡾',
-					'⣼⠏⣿⡏⠄⠄⢠⣤⣶⣶⣾⣿⣿⣟⣾⣾⣼⣿⠒⠄⠄⠄⡠⣴⡄⢠⣿⣵',
-					'⣳⠄⣿⠄⠄⢣⠸⣹⣿⡟⣻⣿⣿⣿⣿⣿⣿⡿⡻⡖⠦⢤⣔⣯⡅⣼⡿⣹',
-					'⡿⣼⢸⠄⠄⣷⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣕⡜⡌⡝⡸⠙⣼⠟⢱⠏',
-					'⡇⣿⣧⡰⡄⣿⣿⣿⣿⡿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣋⣪⣥⢠⠏⠄',
-					'⣧⢻⣿⣷⣧⢻⣿⣿⣿⡇⠄⢀⣀⣀⡙⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠂⠄⠄',
-					'⢹⣼⣿⣿⣿⣧⡻⣿⣿⣇⣴⣿⣿⣿⣷⢸⣿⣿⣿⣿⣿⣿⣿⣿⣰⠄⠄⠄',
-					'⣼⡟⡟⣿⢸⣿⣿⣝⢿⣿⣾⣿⣿⣿⢟⣾⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄⡀⡀',
-					'⣿⢰⣿⢹⢸⣿⣿⣿⣷⣝⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠉⠄⠄⣸⢰⡇',
-					'⣿⣾⣹⣏⢸⣿⣿⣿⣿⣿⣷⣍⡻⣛⣛⣛⡉⠁⠄⠄⠄⠄⠄⠄⢀⢇⡏⠄',
-					'',
-					'Πώς ήταν η μέρα σου, ηλίθιε?',
-				},
-				shortcut = {
-					{ desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-					{
-						icon = ' ',
-						icon_hl = '@variable',
-						desc = 'Files',
-						group = 'Label',
-						action = 'Telescope find_files',
-						key = 'f',
-					},
-					--				{
-					--					desc = " Apps",
-					--					group = "DiagnosticHint",
-					--					action = "Telescope app",
-					--					key = "a",
-					--				},
-					--				{
-					--					desc = " dotfiles",
-					--					group = "Number",
-					--					action = "Telescope dotfiles",
-					--					key = "d",
-					--				},
-				},
-				packages = { enable = true },
-				project = {
-					enable = true,
-					limit = 8,
-					icon = '',
-					label = ' Your awesome projects:',
-					action = 'Telescope find_files cwd=',
-				},
-				mru = { limit = 10, icon = '', label = ' Your recent files:', cwd_only = false },
-				footer = {
-					'wake up',
-				}, -- footer
-			},
+		local alpha = require 'alpha'
+		local dashboard = require 'alpha.themes.dashboard'
+
+		dashboard.section.header.val = {
+			'⣿⠟⣽⣿⣿⣿⣿⣿⢣⠟⠋⡜⠄⢸⣿⣿⡟⣬⢁⠠⠁⣤⠄⢰⠄⠇⢻⢸',
+			'⢏⣾⣿⣿⣿⠿⣟⢁⡴⡀⡜⣠⣶⢸⣿⣿⢃⡇⠂⢁⣶⣦⣅⠈⠇⠄⢸⢸',
+			'⣹⣿⣿⣿⡗⣾⡟⡜⣵⠃⣴⣿⣿⢸⣿⣿⢸⠘⢰⣿⣿⣿⣿⡀⢱⠄⠨⢸',
+			'⣿⣿⣿⣿⡇⣿⢁⣾⣿⣾⣿⣿⣿⣿⣸⣿⡎⠐⠒⠚⠛⠛⠿⢧⠄⠄⢠⣼',
+			'⣿⣿⣿⣿⠃⠿⢸⡿⠭⠭⢽⣿⣿⣿⢂⣿⠃⣤⠄⠄⠄⠄⠄⠄⠄⠄⣿⡾',
+			'⣼⠏⣿⡏⠄⠄⢠⣤⣶⣶⣾⣿⣿⣟⣾⣾⣼⣿⠒⠄⠄⠄⡠⣴⡄⢠⣿⣵',
+			'⣳⠄⣿⠄⠄⢣⠸⣹⣿⡟⣻⣿⣿⣿⣿⣿⣿⡿⡻⡖⠦⢤⣔⣯⡅⣼⡿⣹',
+			'⡿⣼⢸⠄⠄⣷⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣕⡜⡌⡝⡸⠙⣼⠟⢱⠏',
+			'⡇⣿⣧⡰⡄⣿⣿⣿⣿⡿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣋⣪⣥⢠⠏⠄',
+			'⣧⢻⣿⣷⣧⢻⣿⣿⣿⡇⠄⢀⣀⣀⡙⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠂⠄⠄',
+			'⢹⣼⣿⣿⣿⣧⡻⣿⣿⣇⣴⣿⣿⣿⣷⢸⣿⣿⣿⣿⣿⣿⣿⣿⣰⠄⠄⠄',
+			'⣼⡟⡟⣿⢸⣿⣿⣝⢿⣿⣾⣿⣿⣿⢟⣾⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄⡀⡀',
+			'⣿⢰⣿⢹⢸⣿⣿⣿⣷⣝⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠉⠄⠄⣸⢰⡇',
+			'⣿⣾⣹⣏⢸⣿⣿⣿⣿⣿⣷⣍⡻⣛⣛⣛⡉⠁⠄⠄⠄⠄⠄⠄⢀⢇⡏⠄',
+			'',
+			'Πώς ήταν η μέρα σου, ηλίθιε?',
 		}
+
+		dashboard.section.buttons.val = {
+			dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
+			dashboard.button('f', '  > Find file', ':cd $HOME/Documents/projects/personal/ | Telescope find_files<CR>'),
+			dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
+			dashboard.button('n', '󰎚  > Notes', ':cd ~/.nb/home/ | Telescope find_files<CR>'),
+			dashboard.button('q', '  > Quit NVIM', ':qa<CR>'),
+		}
+
+		alpha.setup(dashboard.opts)
 	end,
-	dependencies = { { 'nvim-tree/nvim-web-devicons' } },
 }
