897b3f9876ec26219aa93be4fe27f11b32bfde2b
Vladimir Rubin <vavakado@proton.me>
2024-10-03 02:45:55 +0300

    edgy teen energy

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -39,7 +39,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 vim.keymap.set('n', '<leader>uw', '<cmd>set wrap!<CR>', { desc = 'Toggle [W]rap' })
 
 if vim.g.neovide then
-	vim.o.guifont = 'Hurmit Nerd Font Mono:h15'
+	vim.o.guifont = 'Monoid Nerd Font Mono:h12'
 	vim.g.neovide_scale_factor = 1.0
 	local change_scale_factor = function(delta)
 		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
