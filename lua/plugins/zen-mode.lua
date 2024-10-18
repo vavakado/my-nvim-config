5f73d1afebf6bb90771efa2eee74bf6a22d83e19
Vladimir Rubin <vavakado@proton.me>
2024-10-18 15:30:10 +0300

    zen mode

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		'folke/zen-mode.nvim',
+		keys = {
+			{
+				'<leader>z',
+				'<cmd>ZenMode<CR>',
+				desc = 'Toggle Zen Mode',
+			},
+		},
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+			-- window = {
+			-- 	options = {
+			-- 		signcolumn = 'no', -- disable signcolumn
+			-- 		number = true, -- disable number column
+			-- 		relativenumber = true, -- disable relative numbers
+			-- 		list = true, -- disable whitespace characters
+			-- 	},
+			-- },
+			-- tmux = { enabled = true },
+		},
+	},
+	{
+		'folke/twilight.nvim',
+		cmd = { 'Twilight', 'TwilightEnable', 'TwilightDisable' },
+		lazy = true,
+		opts = {},
+	},
+}
