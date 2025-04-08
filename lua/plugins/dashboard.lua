e1cca599e464e030edf67e7b985012a742ed03ed
Vladimir Rubin <vavakado@proton.me>
2025-04-08 15:18:01 +0300

    quote

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,33 +1,54 @@
 return {
-	'goolord/alpha-nvim',
-	config = function()
-		local alpha = require('alpha')
-		local dashboard = require('alpha.themes.dashboard')
+	{
+		'goolord/alpha-nvim',
+		dependencies = {
+			'RileyGabrielson/inspire.nvim',
+		},
+		config = function()
+			local alpha = require('alpha')
+			local dashboard = require('alpha.themes.dashboard')
+			local inspire = require('inspire')
 
-		dashboard.section.header.val = {
-			'      ⣀⣠⠶⠶⠖⣻⣟⠲⠶⠶⣤⣀      ',
-			'   ⢀⣴⠛⠁   ⢀⡿⢻⡄   ⠈⠛⣦⡀   ',
-			'  ⣴⠛      ⣾⠁ ⣿      ⠛⣦  ',
-			' ⣸⠁      ⢰⠏  ⠸⡇      ⠈⣧⡀',
-			'⢸⡿⣿⣛⠛⠛⠛⠛⢛⡿⠛⠛⠛⠛⢿⡛⠛⠛⠛⠛⣛⣿⢿⡇',
-			'⣼⠁ ⠙⠻⣤⡀ ⣸⠇    ⠘⣧ ⢀⣤⠞⠋  ⣿',
-			'⢻⡀    ⠙⢷⣏⡀     ⣸⡿⠛     ⣿',
-			'⢸⡇    ⢀⡾⠉⠙⢶⣀⣀⡶⠏⠁⢻⡄    ⢸⡇',
-			' ⢻⡀   ⣸⠇ ⢀⣴⠟⠻⢦⡀ ⠘⣧   ⢀⡞⠁',
-			'  ⠻⣤ ⢠⣇⣤⠞⠋    ⠙⠳⣦⣸⡄ ⣤⠟  ',
-			'   ⠈⠻⣿⡋⠁         ⢙⣿⠟⠁   ',
-			'      ⠉⠻⠶⠶⣤⣤⣤⣤⠶⠶⠞⠉       ',
-		}
+			local header = {
+				'           ⣀⣠⠶⠶⠖⣻⣟⠲⠶⠶⣤⣀      ',
+				'        ⢀⣴⠛⠁   ⢀⡿⢻⡄   ⠈⠛⣦⡀   ',
+				'       ⣴⠛      ⣾⠁ ⣿      ⠛⣦  ',
+				'      ⣸⠁      ⢰⠏  ⠸⡇      ⠈⣧⡀',
+				'     ⢸⡿⣿⣛⠛⠛⠛⠛⢛⡿⠛⠛⠛⠛⢿⡛⠛⠛⠛⠛⣛⣿⢿⡇',
+				'     ⣼⠁ ⠙⠻⣤⡀ ⣸⠇    ⠘⣧ ⢀⣤⠞⠋  ⣿',
+				'     ⢻⡀    ⠙⢷⣏⡀     ⣸⡿⠛     ⣿',
+				'     ⢸⡇    ⢀⡾⠉⠙⢶⣀⣀⡶⠏⠁⢻⡄    ⢸⡇',
+				'      ⢻⡀   ⣸⠇ ⢀⣴⠟⠻⢦⡀ ⠘⣧   ⢀⡞⠁',
+				'       ⠻⣤ ⢠⣇⣤⠞⠋    ⠙⠳⣦⣸⡄ ⣤⠟  ',
+				'        ⠈⠻⣿⡋⠁         ⢙⣿⠟⠁   ',
+				'           ⠉⠻⠶⠶⣤⣤⣤⣤⠶⠶⠞⠉      ',
+			}
+			local quote = inspire.get_quote()
+			local centered_quote = inspire.center_text(quote.text, quote.author, 34, 10, 34)
 
-		dashboard.section.buttons.val = {
-			dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
-			dashboard.button('f', '󰈞  > Find file', ':cd $HOME/Development/projects/personal/ | Telescope find_files<CR>'),
-			dashboard.button('p', '  > Projects', require('telescope').extensions.projects.projects),
-			dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
-			dashboard.button('n', '󰎚  > Notes', ':cd ~/Documents/notes/ | Telescope find_files<CR>'),
-			dashboard.button('q', '󰅙  > Quit NVIM', ':qa<CR>'),
-		}
+			for _, line_text in pairs(centered_quote) do
+				table.insert(header, line_text)
+			end
 
-		alpha.setup(dashboard.opts)
-	end,
+			dashboard.section.header.val = header
+			dashboard.section.buttons.val = {
+				dashboard.button('e', '  > New file', ':ene <BAR> startinsert <CR>'),
+				dashboard.button('f', '󰈞  > Find file', ':cd $HOME/Development/projects/personal/ | Telescope find_files<CR>'),
+				dashboard.button('p', '  > Projects', require('telescope').extensions.projects.projects),
+				dashboard.button('r', '  > Recent', ':Telescope oldfiles<CR>'),
+				dashboard.button('n', '󰎚  > Notes', ':cd ~/Documents/notes/ | Telescope find_files<CR>'),
+				dashboard.button('q', '󰅙  > Quit NVIM', ':qa<CR>'),
+			}
+
+			alpha.setup(dashboard.opts)
+		end,
+	},
+	{
+		{
+			'RileyGabrielson/inspire.nvim',
+			config = function()
+				require('inspire').setup({})
+			end,
+		},
+	},
 }
