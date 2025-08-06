265c51585c4de45ccaa57fbc64ec3fcfb4c0fe69
Vladimir Rubin <xor@vavakado.xyz>
2025-08-06 19:03:07 +0300

    supermaven return

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,13 @@
+return {
+	'supermaven-inc/supermaven-nvim',
+	config = function()
+		require('supermaven-nvim').setup({
+			ignore_filetypes = { 'md', 'markdown', 'org', 'text' },
+			keymaps = {
+				accept_suggestion = '<C-G>',
+				clear_suggestion = '<C-]>',
+				accept_word = '<C-j>',
+			},
+		})
+	end,
+}
